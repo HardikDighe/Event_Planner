54c9307d84a148282f4b13b5e5c142a82bf7ba55
ashwininaher <aherashwini101@gmail.com>
2024-08-05 10:49:04 +0530

    add

--- Event-Planner/app/screens/All_Invoice/container/index.tsx
+++ Event-Planner/app/screens/All_Invoice/container/index.tsx
@@ -1,22 +1,29 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import AllInvoices from "../../../../app/screens/All_Invoice/components/AllInvoice"; // Adjust the path if necessary
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AllInvoices from "../../../../app/screens/All_Invoice/components/AllInvoice"; // Adjust the import path if needed
+import CreateInvoice from "../../../../app/screens/All_Invoice/components/CreateInvoice"; // Adjust the import path if needed
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
-const App = () => {
+function App() {
   return (
     <NavigationContainer independent={true}>
       <Stack.Navigator>
         <Stack.Screen
           name="AllInvoices"
           component={AllInvoices}
-          options={{ headerShown: false }} // Hide the default header
+          options={{ headerShown: false }}
         />
+        <Stack.Screen
+          name="CreateInvoice"
+          component={CreateInvoice}
+          options={{ headerShown: false }}
+        />
+        {/* Add other screens here */}
       </Stack.Navigator>
     </NavigationContainer>
   );
-};
+}
 
 export default App;
