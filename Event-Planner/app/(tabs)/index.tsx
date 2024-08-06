a6d1e950de2b9800c49f829f9212ca002ca82f33
HardikDighe <hardikdighe2001@gmail.com>
2024-08-06 11:05:19 +0530

    Add All Events Screen

--- Event-Planner/app/(tabs)/index.tsx
+++ Event-Planner/app/(tabs)/index.tsx
@@ -1,11 +1,1 @@
-import LoginPageNavigation from "../../app/screens/Login/components/LoginPageNavigation";
-
-LoginPageNavigation
-
-export default function HomeScreen() {
-  return (
-    
-    <LoginPageNavigation/>
-  
-  );
-}
+//
