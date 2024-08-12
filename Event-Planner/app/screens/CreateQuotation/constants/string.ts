4323bf3dee651ad6ecdd3548abee2ea764087ac9
Yogesh22032001 <yogeshmanikeri2203@gmail.com>
2024-08-12 11:22:59 +0530

    added constants for create Quotation

---
+++ Event-Planner/app/screens/CreateQuotation/constants/string.ts
@@ -0,0 +1,7 @@
+export const HEADERS = {
+    "Content-Type": "application/json"
+};
+export const ERROR_MESSAGES = {
+    saveFailed: 'Failed to save quotation.',
+    saveError: 'An error occurred while saving the quotation.'
+};
